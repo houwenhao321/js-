5138a28fe1dbe7f5a9fb8958481e94a697f12283
houwenhao321 <2942573925@qq.com>
2022-12-29 13:43:27 +0800

    ✨ feat: 百度的api封装

---
+++ 百度api/main.js
@@ -0,0 +1,3 @@
+import api from './http/index'
+Vue.use(api)
+// 此时可以直接在 Vue 原型上调用 $api 了
---
+++ 百度api/views/test.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            
+        }
+    },
+    methods: {
+        apiTestFun(){
+            this.$api.list(id).then(res => {
+            if (res.rc === 0) {
+                this.pageList = res.data.item
+            } else {
+                this.$Message.info(res.desc);
+            }
+            })
+            .catch(error => {
+                this.$Message.info(error);
+            })
+        },  
+    },
+}
+</script>
